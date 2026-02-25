ca7fa331fa56db3b0a1d6181f7bcc2c1ff49de8b
harsh-hak <harsh-hak@users.noreply.github.com>
2026-02-25 18:27:18 +0000

    deploy: 8c65d48b632a1c72634b16607b3cda3ef870fb58

--- assets/CallForSpeakers-BQRSqc0H.js
+++ assets/CallForSpeakers-BQRSqc0H.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-CS4sctz6.js";import{r as d}from"./vendor-react-CZ_vUJpe.js";import{k as m,j as p,l as x}from"./vendor-icons-2cbBA4y1.js";const f=()=>{const[a,o]=d.useState({name:"",email:"",topic:"",abstract:""}),l=t=>{const{name:s,value:r}=t.target;o(c=>({...c,[s]:r}))},n=t=>{t.preventDefault();const s=encodeURIComponent(`Speaker Proposal: ${a.topic}`),r=encodeURIComponent(`Name: ${a.name}
+import{j as e}from"./index-CUMKkC24.js";import{r as d}from"./vendor-react-CZ_vUJpe.js";import{j as m,i as p,k as x}from"./vendor-icons-D54-4r0P.js";const f=()=>{const[a,o]=d.useState({name:"",email:"",topic:"",abstract:""}),l=t=>{const{name:s,value:r}=t.target;o(c=>({...c,[s]:r}))},n=t=>{t.preventDefault();const s=encodeURIComponent(`Speaker Proposal: ${a.topic}`),r=encodeURIComponent(`Name: ${a.name}
 Email: ${a.email}
 
 Topic: ${a.topic}
